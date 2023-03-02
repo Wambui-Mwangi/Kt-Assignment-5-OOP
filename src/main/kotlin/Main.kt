75b48bf15d6d2c821fc91f6b61ceef40acd05c52
Esther <mwangiestherwambuii@gmail.com>
2023-03-02 08:13:56 +0300

    OOP Assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    val humanbeing = Human("Esther", 24, 58)
+    humanbeing.eat(2)
+    println(humanbeing.weight)
+
+    humanbeing.speak("Dollars before Oxygen")
+
+    val accountCreation = User("Esther", "Mwangi", "mwangiesther@gmail.com", "0701378907", "Yaaay#4")
+    println(accountCreation.email)
+    println(accountCreation.phoneNumber)
+
+}
+class Human(var name:String, var age:Int, var weight:Int){
+    fun eat(foodWeight:Int){
+        weight += foodWeight
+        println("I am eating $foodWeight kgs of food")
+    }
+    fun speak(speech:String){
+        println(speech)
+    }
+    fun birthday(birthdate:Int){
+        age+= birthdate
+    }
+}
+data class User(var firstName:String, var lastname:String, var email:String, var phoneNumber:String, var password:String)
